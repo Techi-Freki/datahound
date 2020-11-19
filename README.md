847693cafb0d1eb24235f0e4776fd0680b0f838f
D. Bryan Combs <dbcombs@gmail.com>
2020-11-18 19:49:01 -0500

    Adding datahound project to repository.

---
+++ README.md
@@ -0,0 +1,113 @@
+#Datahound
+
+SqLite data access layer for python.
+
+##Methods
+
+* fetchone(sql: str, *args) -> tuple
+* fetchmany(amount: int, sql: str, *args) -> list
+* fetchall(sql: str, *args) -> list
+* insert_return_id(sql: str, *args) -> int
+* insert_many(sql: str, *args) -> None
+* execute(sql: str, *args) -> None
+* execute_scripts(sql: str) -> None
+* execute_return_id(sql: str, *args) -> int (deprecated)
+
+##Usage
+
+###Extending Provider Base
+
+    from datahound import DataProviderBase
+    
+    
+    # extend the provider base class
+    class DataProviderOne(DataProviderBase):
+        db_path = '/path/to/database/test.db'
+        
+        def __init__(self):
+            super().__init__(DataProviderOne.db_path)
+    
+    
+    class DataProviderTwo(DataProviderBase):
+        db_path = '../alternate/data/test.db
+        
+        def __init__(self):
+            super().__init__(DataProviderTwo.db_path)
+    
+    
+    class AppDataProvider(object):
+        test_data_one = new DataProviderOne()
+        test_data_two = new DataProviderTwo()
+    
+###Running Queries
+
+    def get_data():
+        sql = 'SELECT id, name, age FROM user;'
+        results = AppDataProvider.test_data_one.fetchall(sql)
+        
+        return list(results)
+    
+    
+    get_data()
+    >> [(1, 'Mark', 25), (2, 'Jane', 31), (3, 'Steve', 45)]
+    
+    def return_id(name, age):
+        sql = 'INSERT INTO user (name, age) VALUES (?, ?)'
+        parameters = (name, age)
+        return AppDataProvider.test_data_one.insert_return_id(sql, *parameters)
+    
+    
+    return_id('Angela', 27)
+    >> 4
+    
+    get_data()
+    >> [(1, 'Mark', 25), (2, 'Jane', 31), (3, 'Steve', 45), (4, 'Angela', 27)]
+    
+    
+###Parameterized Data
+    
+    def get_record(id):
+        sql = 'SELECT id, name, age FROM user WHERE id = ?'
+        result = AppDataProvider.test_data_one.fetchone(sql, id)
+        
+        return result
+
+
+    def insert_record(user):
+        sql = 'INSERT INTO user (name, age) VALUES (?, ?)'
+        parameters = (user.name, user.age)
+        AppDataProvider.test_data_one.execute(sql, *parameters)
+        
+        
+    get_record(1)
+    >> (1, 'Mark', 25)
+
+##Changelog
+
+1.1.1
+
+* Refactored code to improve performance and comply with PEP.
+* Updated README.md formatting.
+
+1.1.0
+
+* Added **insert_many** and **execute_scripts** to methods.
+* Updated README.md and cleaned up private method in package.
+
+1.0.2
+
+* Added **insert_return_id** and deprecated **execute_return_id** due to readability issues.
+* Removed extra space in the setup.py file.
+
+1.0.1
+
+* Added MANIFEST.in and updated setup.py.
+* Updated README.md to give clearer sql statements in the usage section.
+
+1.0.0
+
+* Added **execute_return_id** method, a method that returns the last inserted id after a successful insert.
+
+1.0.0RC1
+
+* Initial Release.
---
+++ datahound/tests/DataProviderBase.py
@@ -0,0 +1,199 @@
+import unittest
+import warnings
+import os
+
+from datahound import DataProviderBase
+
+
+table_name = 'test_table'
+
+
+class DataProvider(DataProviderBase):
+    relative_path = 'db/tests.sqlite3'
+    db_path = f'{os.path.dirname(os.path.abspath(__file__))}/{relative_path}'
+
+    def __init__(self):
+        super().__init__(DataProvider.db_path)
+
+    def __del__(self):
+        self.db_path = None
+
+
+class FalseProvider(DataProviderBase):
+    db_path = 'db/error_db.txt'
+
+    def __init__(self):
+        super().__init__(FalseProvider.db_path)
+
+    def __del__(self):
+        self.db_path = None
+
+
+class Helper(object):
+    @staticmethod
+    def setup():
+        data_provider = DataProvider()
+        sql = f'create table if not exists {table_name} (' \
+            'id integer not null primary key autoincrement, ' \
+            'name varchar(32) not null)'
+        data_provider.execute(sql)
+
+    @staticmethod
+    def cleanup():
+        data_provider = DataProvider()
+        sql = f'drop table {table_name}'
+        data_provider.execute(sql)
+
+    @staticmethod
+    def truncate_table(table):
+        data_provider = DataProvider()
+        sql = f'delete from {table}'
+        data_provider.execute(sql)
+
+
+class DataProviderBaseTest(unittest.TestCase):
+    def test_connection_error(self):
+        sql = f'select * from {table_name}'
+        provider = FalseProvider()
+        self.assertRaises(Exception, lambda: provider.execute(sql))
+
+    def test_executeCreate(self):
+        Helper.setup()
+        data_provider = DataProvider()
+
+        sql = f"select name from sqlite_master where type='table' and name='{table_name}'"
+        actual = data_provider.fetchall(sql)
+        self.assertGreater(len(actual), 0)
+
+    def test_executeInsertTruncate(self):
+        Helper.truncate_table(table_name)
+        sql = f"insert into {table_name} (name) values ('datatest1')"
+        data_provider = DataProvider()
+        data_provider.execute(sql)
+
+        actual_sql = f'select * from {table_name}'
+        actual = data_provider.fetchall(actual_sql)
+        self.assertGreater(len(actual), 0)
+        self.assertEqual(actual[0][1], 'datatest1')
+        Helper.truncate_table(table_name)
+        cleaned = data_provider.fetchall(actual_sql)
+        self.assertEqual(len(cleaned), 0)
+
+    def test_fetchall(self):
+        Helper.truncate_table(table_name)
+        sql = f"insert into {table_name} (name) values ('booger'), ('testing')"
+        data_provider = DataProvider()
+        data_provider.execute(sql)
+
+        actual_sql = f'select * from {table_name}'
+        actual = data_provider.fetchall(actual_sql)
+        self.assertEqual(len(actual), 2)
+        Helper.truncate_table(table_name)
+        self.assertEqual(len(data_provider.fetchall(actual_sql)), 0)
+
+    def test_fetchmany(self):
+        sql = f"insert into {table_name} (name) values ('boogers'), ('boogers1'), ('boogers3'), ('boogers4')"
+        data_provider = DataProvider()
+        data_provider.execute(sql)
+
+        actual_sql = f"select * from {table_name} where name like '%boogers%'"
+        actual = data_provider.fetchmany(3, actual_sql)
+        self.assertEqual(len(actual), 3)
+        Helper.truncate_table(table_name)
+
+    def test_fetchone(self):
+        sql = f"insert into {table_name} (name) values ('fetchone')"
+        data_provider = DataProvider()
+        data_provider.execute(sql)
+
+        actual_sql = f'select * from {table_name} limit 1'
+        actual = data_provider.fetchone(actual_sql)
+        self.assertIsNotNone(actual)
+        Helper.truncate_table(table_name)
+        self.assertIsNone(data_provider.fetchone(actual_sql))
+
+    def test_execute_scripts(self):
+        sql = f"create table if not exists {table_name} (id integer not null primary key autoincrement," \
+              f" name varchar(64) not null); insert into {table_name} (name) values ('testing1'), ('testing2')"
+        data_provider = DataProvider()
+        data_provider.execute_scripts(sql)
+
+        actual_sql = f'select * from {table_name}'
+        actual = data_provider.fetchall(actual_sql)
+        self.assertIsNotNone(actual)
+        self.assertEqual(len(actual), 2)
+        data_provider.execute(f'drop table {table_name}')
+
+    def test_execute_scripts_fail(self):
+        sql = f"create table if not exists {table_name} (id integer not null primary key autoincrement," \
+              " name varchar(64) not null)"
+        data_provider = DataProvider()
+        self.assertRaises(Exception, lambda: data_provider.execute_scripts(sql))
+
+    def test_fetchallwithparameters(self):
+        data_provider = DataProvider()
+        sql = f'insert into {table_name} (name) values (?), (?)'
+        data_provider.execute(sql, 'test1', 'test2')
+
+        actual_sql = f'select name from {table_name} where name = ?'
+        actual = data_provider.fetchall(actual_sql, 'test1')
+        self.assertEqual(len(actual), 1)
+        self.assertEqual([('test1',)], actual)
+        Helper.truncate_table(table_name)
+
+    def test_executereturnid(self):
+        Helper.setup()
+        data_provider = DataProvider()
+        sql = f'insert into {table_name} (name) values (?)'
+        last_id = data_provider.execute_return_id(sql, 'testing')
+
+        self.assertTrue(type(last_id) == int)
+        Helper.truncate_table(table_name)
+
+    def test_executereturnid_deprecation(self):
+        with warnings.catch_warnings(record=True) as warning:
+            warnings.simplefilter('always')
+            Helper.setup()
+            data_provider = DataProvider()
+            sql = f'insert into {table_name} (name) values (?)'
+            data_provider.execute_return_id(sql, 'testing')
+
+            self.assertTrue(len(warning) != 0)
+            self.assertTrue(issubclass(warning[-1].category, DeprecationWarning))
+
+    def test_insertreturnid(self):
+        Helper.setup()
+        data_provider = DataProvider()
+        sql = f'insert into {table_name} (name) values (?)'
+        last_id = data_provider.insert_return_id(sql, 'testing')
+
+        self.assertTrue(type(last_id) == int)
+        Helper.truncate_table(table_name)
+
+    def test_insertreturnid_fail(self):
+        Helper.setup()
+        data_provider = DataProvider()
+        sql = f"update {table_name} set name = 'test' where id = 1"
+
+        self.assertRaises(Exception, lambda: data_provider.insert_return_id(sql))
+
+    def test_insertmany(self):
+        data_provider = DataProvider()
+        sql = f'INSERT INTO {table_name} (name) VALUES (?)'
+        parameters = ('Test1',), ('Test2',), ('Test3',)
+        data_provider.insert_many(sql, *parameters)
+
+        sql = f'SELECT name FROM {table_name} WHERE name IN (\'Test1\', \'Test2\', \'Test3\')'
+        records = data_provider.fetchall(sql)
+
+        for record in records:
+            self.assertTrue(record in parameters)
+
+        Helper.truncate_table(table_name)
+
+    def test_insertmany_fail(self):
+        data_provider = DataProvider()
+        sql = f'UPDATE {table_name} SET name = \'Testing\' WHERE id = 0'
+        parameters = ('Test1',), ('Test2',), ('Test3',)
+
+        self.assertRaises(Exception, lambda: data_provider.insert_many(sql, *parameters))
